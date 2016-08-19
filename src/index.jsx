525b6077fcf71441928aa67fb80b04d3db88b3fa
DESKTOP-DQ7EVPG\ALEX-CG <a.contrerasgranados@gmail.com>
2016-08-19 08:31:51 -0500

    Configuracion webpack express

--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import CookbookApp from './components/cookbookapp/index.jsx';
 
 ReactDOM.render(<CookbookApp />, document.getElementById('container'));
+
+console.log('Loaded!!!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,63 @@
 var webpack = require('webpack');
 var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 
 var src_dir = path.resolve(__dirname, 'src');
 var out_dir = path.resolve(__dirname, 'build');
 
-var config = {
-	entry: src_dir + '/index.jsx',
-	output: {
-		path: out_dir,
-		filename: 'app.js'
+var config = [
+	{
+		name: 'client',
+		entry: './src',
+		output: {
+			path: out_dir,
+			filename: 'app.js'
+		},
+		module:{
+			loaders: [{
+				include: src_dir,
+				exclude: [nodeModulesPath],
+				loader: 'babel',
+	            query: {
+	                presets: ['es2015']
+	            }
+			}]
+		},
+		resolve: {
+			extensions: ['.json','.jsx']
+		},
+		node: {
+        	fs: "empty"
+   		}
 	},
-	module:{
-		loaders: [{
-			include: src_dir,
-			exclude: /node_modules/,
-			loader: 'babel'
-		}]
+	{
+		name: 'server',
+		entry: [
+			'./src/server',
+			'./src/util'
+		],
+		output: {
+			path: out_dir,
+			filename: 'server.js'
+		},
+		module:{
+			loaders: [{
+				include: src_dir,
+				exclude: [nodeModulesPath],
+				loader: 'babel',
+	            query: {
+	                presets: ['es2015']
+	            }
+			}]
+		},
+		resolve: {
+			extensions: ['.json','.jsx']
+		},
+		node: {
+        	fs: "empty"
+   		}
 	}
-};
+
+];
 
 module.exports = config;
